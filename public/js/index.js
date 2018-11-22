2f3936f1f3378fa6b0a8f15f5260970c6adafa8c
AkashJawale <akashjawale131@gmail.com>
2018-11-23 00:57:00 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,7 @@ var socket = io();
 	socket.on('connect', function() {
 		console.log('Connected to server');
 
-		socket.emit('createMessage', {
-			from: 'Akash',
-			text: 'Yup, that works for me.'
-		});
-	});
-
+	});	
 	socket.on('disconnect', function() {
 		console.log('Disconnected from server');
 	});
